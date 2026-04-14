f84dcc8326eefe13fd0f387e09a5944837b84fc0
Ana Lopez <avantadesignlab@gmail.com>
2026-04-14 18:58:36 +0000

    fix: minor corrections

--- src/pages/AddContact.jsx
+++ src/pages/AddContact.jsx
@@ -22,22 +22,22 @@ export const AddContact = () => {
 
             <div className="col-12">
                 <label htmlfor="inputName" className="form-label">Name</label>
-                <input type="text" className="form-control" id="inputName" value {data.name} onChange={formChange} name="email"/>
+                <input type="text" className="form-control" id="inputName" value={data.name} onChange={formChange} name="name"/>
             </div>
 
             <div className="col-md-6">
-                <label htmlfor="inputPassword4" className="form-label">Phone</label>
-                <input type="texto" className="form-control" id="inputPhone" value {data.phone} onChange={formChange}/>
+                <label htmlFor="inputPassword4" className="form-label">Phone</label>
+                <input type="texto" className="form-control" id="inputPhone" value={data.phone} onChange={formChange} name="phone"/>
             </div>
 
             <div className="col-md-6">
-                <label htmlfor="inputEmail4" className="form-label">Email</label>
-                <input type="email" className="form-control" id="inputEmail4" value {data.email} onChange={formChange}/>
+                <label htmlFor="inputEmail4" className="form-label">Email</label>
+                <input type="email" className="form-control" id="inputEmail4" value={data.email} onChange={formChange} name="email"/>
             </div>
 
             <div className="col-12">
-                <label htmlfor="inputAddress" className="form-label">Address</label>
-                <input type="text" className="form-control" id="inputAddress" value {data.address} onChange={formChange}placeholder="1234 Main St" />
+                <label htmlFor="inputAddress" className="form-label">Address</label>
+                <input type="text" className="form-control" id="inputAddress" value={data.address} onChange={formChange} name="address" />
             </div>
 
             <div className="col-12">
